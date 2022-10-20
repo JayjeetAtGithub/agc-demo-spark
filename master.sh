e6e89e3fcccd43d766be1e38a23fcd6e5c2fa907
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-20 14:23:43 -0700

    add scripts

---
+++ master.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -ex
+
+wget https://dlcdn.apache.org/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
+tar -xvf spark-3.3.0-bin-hadoop3.tgz
+cd spark-3.3.0-bin-hadoop3
+
+./sbin/start-master.sh -h $1
+
+echo "Connect workers to spark://$1:7077"
