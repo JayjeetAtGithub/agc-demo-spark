9f45ede0350f85bb090de3f1191235987bddc5cc
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-10-27 09:56:55 -0700

    update scripts with Spark 3.3.1 release and add a README

---
+++ README.md
@@ -0,0 +1,12 @@
+# iceberg-spark-hep
+
+```
+apt update
+apt install -y default-jre default-jdk
+
+git clone https://github.com/JayjeetAtGithub/iceberg-spark-hep
+cd iceberg-spark-hep/
+
+
+```
+
--- master.sh
+++ master.sh
@@ -3,7 +3,7 @@ set -ex
 
 rm -rf spark*
 
-wget https://dlcdn.apache.org/spark/spark-3.3.0/spark-3.3.0-bin-hadoop3.tgz
+wget https://dlcdn.apache.org/spark/spark-3.3.1/spark-3.3.1-bin-hadoop3.tgz
 tar -xvf spark-3.3.0-bin-hadoop3.tgz
 cd spark-3.3.0-bin-hadoop3
 
